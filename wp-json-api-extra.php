3f7370aac2ea3af78b0db4cce5229b06b234eba1
Zeno Zeng <zenoes@qq.com>
2013-08-06 19:44:57 +0800

    first commit

---
+++ wp-json-api-extra.php
@@ -0,0 +1,55 @@
+<?php
+/*
+  Plugin Name: JSON API Extra
+  Plugin URI: https://github.com/zenozeng/wp-json-api-extra/
+  Description: Yet Some ugly but useful API for Wordpress
+  Version: 0.0.2
+  Author: Zeno Zeng
+  Author: http://zenoes.com
+  License: GNU General Public License Version 3
+*/
+
+class JSON_API_Extra {
+    static function install() {
+        add_option('json_api_extra_site_last_modified', 0);
+        self::update_last_modified();
+    }
+    static function update_last_modified() {
+        update_option('json_api_extra_site_last_modified', time());
+    }
+    static function get_last_modified() {
+        return get_option('json_api_extra_site_last_modified');
+    }
+}
+
+function json_api_extra() {
+    $data = '';
+    if(isset($_GET) && isset($_GET['jsonextra'])) {
+        switch ($_GET['jsonextra']) {
+            
+          case 'lastModified':
+              # the timestamp of the last modifition of this site
+              $data = array('lastModified' => JSON_API_Extra::get_last_modified());
+              break;
+
+        }
+        $json = json_encode($data);
+        if (isset($_GET['callback'])) {
+            $callback = $_GET['callback'];
+            header('Content-Type: application/javascript');
+            echo "$callback($json)";
+        } else {
+            echo $json;
+        }
+        exit();
+    }
+}
+
+add_action('init', 'json_api_extra');
+register_activation_hook(__FILE__, array('JSON_API_Extra', 'install'));
+
+// http://codex.wordpress.org/Plugin_API/Action_Reference # Post, Page, Attachment, and Category Actions (Admin) 
+$hooks = array('create_category', 'delete_category', 'trashed_post', 'deleted_post', 'edit_category', 'save_post');
+foreach($hooks as $hook) {
+    add_action($hook, 'JSON_API_Extra::update_last_modified');
+}
